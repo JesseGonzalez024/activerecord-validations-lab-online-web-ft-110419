044de0bf195727dfbf6b1ebf86cdc10029fcfa44
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-30 03:02:06 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :name, length: {minumum }
+  validates :name, length: { minimum: 250 }
   validates :content, length: {is: 10 }
 end
